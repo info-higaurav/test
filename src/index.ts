ef5367c71dbdc85490d841c8982a1879d72097dd
ai-higaurav <ai.higaurav@gmail.com>
2024-10-08 07:55:51 +0530

    aws key added

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,5 @@ app.use('/',userRoutes)
 app.use('/admin', adminRoutes)
 
 app.listen(3000,()=>{
-    console.log('Server Status is OK updat')
+    console.log('Server Status is OK aws key added')
 })
